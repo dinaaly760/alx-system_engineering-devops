15e641639cc005e3e11198b24cebb23ec10ffa1d
Dina Aly <dinaaly760@gmail.com>
2024-07-22 23:09:38 -0700

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,11 @@
+# set up your client SSH configuration file so that you can connect to a server without typing a password.
 file_line { 'Turn off passwd auth':
-  path  => '~/.ssh/config',
-  line  => 'PasswordAuthentication no',
-}
-
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+  }
 file_line { 'Declare identity file':
-  path  => '~/.ssh/config',
-  line  => 'IdentityFile ~/.ssh/school',
-}
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/holberton',
+  }
