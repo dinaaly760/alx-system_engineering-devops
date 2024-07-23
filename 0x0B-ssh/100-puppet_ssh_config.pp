9c5701ec7eb5d489b6000298fcd3eb2849a9c0d4
Dina Aly <dinaaly760@gmail.com>
2024-07-22 22:56:38 -0700

    ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,9 @@
+file_line { 'Turn off passwd auth':
+  path  => '~/.ssh/config',
+  line  => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  path  => '~/.ssh/config',
+  line  => 'IdentityFile ~/.ssh/school',
+}
